b737e70a47e1530168c2d3635c01cbc6af810cb7
Felipe Ambrosi <91692104+C0rvj4@users.noreply.github.com>
2023-04-29 13:44:42 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,11 +14,7 @@ namespace Projeto1
         [STAThread]
         static void Main(String[] args)
         {
-            Gerente Felipe = new Gerente("Felipe", "127.577.419-93", "48991342824", "Fe25022003");
-
-            Console.WriteLine(Felipe.Senha);
-
-            Console.ReadLine();
+             
         }
 
 
