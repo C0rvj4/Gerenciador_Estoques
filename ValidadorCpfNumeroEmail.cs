218a4812fb3d43f2d452ac5120e2c54f2d205bbb
Felipe Ambrosi <91692104+C0rvj4@users.noreply.github.com>
2023-05-19 08:03:47 -0300

    Update ValidadorCpfNumeroEmail.cs

--- ValidadorCpfNumeroEmail.cs
+++ ValidadorCpfNumeroEmail.cs
@@ -1,43 +1,66 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gerencia1;
 
-namespace Gerencia
+namespace Gerencia1
 {
+
+
     public class Validador
     {
 
-    public bool ValidarCpf(string cpf)
-{
-    cpf = cpf.Replace(".", "").Replace("-", "");
+        public static bool ValidarCpf(string cpf)
+        {
+            if (string.IsNullOrWhiteSpace(cpf))
+                throw new ArgumentNullException(nameof(cpf), "O CPF não pode ser nulo ou vazio.");
 
-    if (cpf.Length != 11 || !long.TryParse(cpf, out long n))
-        return false;
+            cpf = cpf.Replace(".", "").Replace("-", "");
 
-    long soma = 0;
-    for (int i = 0; i < 9; i++)
-        soma += (cpf[i] - '0') * (10 - i);
+            if (cpf.Length != 11 || !long.TryParse(cpf, out long n))
+                return false;
 
-    long resto = soma % 11;
-    long digitoVerificador = resto < 2 ? 0 : 11 - resto;
+            long soma = 0;
+            for (int i = 0; i < 9; i++)
+                soma += (cpf[i] - '0') * (10 - i);
 
-    if (digitoVerificador != cpf[9] - '0')
-        return false;
+            long resto = soma % 11;
+            long digitoVerificador = resto < 2 ? 0 : 11 - resto;
 
-    soma = 0;
-    for (int i = 0; i < 10; i++)
-        soma += (cpf[i] - '0') * (11 - i);
+            if (digitoVerificador != cpf[9] - '0')
+                return false;
 
-    resto = soma % 11;
-    digitoVerificador = resto < 2 ? 0 : 11 - resto;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += (cpf[i] - '0') * (11 - i);
 
-    return digitoVerificador == cpf[10] - '0';
-}
+            resto = soma % 11;
+            digitoVerificador = resto < 2 ? 0 : 11 - resto;
 
-public static bool ValidarTelefone(string telefone)
-{
-    telefone = telefone.Replace("(", "").Replace(")", "").Replace(" ", "").Replace("-", "");
-    if (telefone.Length != 11 || !long.TryParse(telefone, out long n))
-        return false;
-    return true;
-}
+            return digitoVerificador == cpf[10] - '0';
+
+        }
+
+
+
+        public static bool ValidarTelefone(string telefone)
+        {
+
+            if (string.IsNullOrWhiteSpace(telefone)) 
+                throw new ArgumentNullException(nameof(telefone), "O contato não pode ser nulo ou vazio.");
+
+            telefone = telefone.Replace("(", "").Replace(")", "").Replace(" ", "").Replace("-", "");
+            if (telefone.Length != 11 || !long.TryParse(telefone, out long n))
+                return true;
+
+            else
+            {
+                throw new ArgumentException(nameof(telefone), "O Número de telefone não é valido, verifique o número inserido e tente novamente.");
+
+            }
+        }
+
     }
 }
